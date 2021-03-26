777bb06a6f03dad4b971eea3371c910b47c8304e
Li-zg <1115069891@qq.com>
2021-03-26 15:42:58 +0800

    初始化项目

---
+++ src/main/java/com/aoxzon/demo/AoxzonApplication.java
@@ -0,0 +1,13 @@
+package com.aoxzon.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AoxzonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AoxzonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aoxzon/demo/AoxzonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aoxzon.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AoxzonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
